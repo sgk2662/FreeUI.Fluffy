35599e9f0c98905e9b025008a20b4bc13c47e813
solor <Naushika@outlook.com>
2016-10-27 19:10:25 +0800

    fix coords error

--- Interface/AddOns/FreeUI/scripts/maps/map.lua
+++ Interface/AddOns/FreeUI/scripts/maps/map.lua
@@ -87,36 +87,38 @@ local freq = C.performance.mapcoords
 local last = 0
 
 WorldMapDetailFrame:HookScript("OnUpdate", function(self, elapsed)
-	last = last + elapsed
-	if last >= freq then
-		local x, y = GetPlayerMapPosition("player")
-		x = math.floor(100 * x)
-		y = math.floor(100 * y)
-		if x ~= 0 and y ~= 0 then
-			coords:SetText(PLAYER..": "..x..", "..y)
-			cursorcoords:SetPoint("BOTTOMLEFT", coords, "TOPLEFT", 0, 4)
-		else
-			coords:SetText("")
-			cursorcoords:SetPoint("BOTTOMLEFT", UIFrame, 5, 5)
+	if select(2, GetInstanceInfo()) == "none" then
+		last = last + elapsed
+		if last >= freq then
+			local x, y = GetPlayerMapPosition("player")
+			x = math.floor(100 * x)
+			y = math.floor(100 * y)
+			if x ~= 0 and y ~= 0 then
+				coords:SetText(PLAYER..": "..x..", "..y)
+				cursorcoords:SetPoint("BOTTOMLEFT", coords, "TOPLEFT", 0, 4)
+			else
+				coords:SetText("")
+				cursorcoords:SetPoint("BOTTOMLEFT", UIFrame, 5, 5)
+			end
+
+			local scale = WorldMapDetailFrame:GetEffectiveScale()
+			local width = WorldMapDetailFrame:GetWidth()
+			local height = WorldMapDetailFrame:GetHeight()
+			local centerX, centerY = WorldMapDetailFrame:GetCenter()
+			local x, y = GetCursorPosition()
+			local adjustedX = (x / scale - (centerX - (width/2))) / width
+			local adjustedY = (centerY + (height/2) - y / scale) / height
+
+			if (adjustedX >= 0  and adjustedY >= 0 and adjustedX <= 1 and adjustedY <= 1) then
+				adjustedX = math.floor(100 * adjustedX)
+				adjustedY = math.floor(100 * adjustedY)
+				cursorcoords:SetText(MOUSE_LABEL..": "..adjustedX..", "..adjustedY)
+			else
+				cursorcoords:SetText(" ")
+			end
+
+			last = 0
 		end
-
-		local scale = WorldMapDetailFrame:GetEffectiveScale()
-		local width = WorldMapDetailFrame:GetWidth()
-		local height = WorldMapDetailFrame:GetHeight()
-		local centerX, centerY = WorldMapDetailFrame:GetCenter()
-		local x, y = GetCursorPosition()
-		local adjustedX = (x / scale - (centerX - (width/2))) / width
-		local adjustedY = (centerY + (height/2) - y / scale) / height
-
-		if (adjustedX >= 0  and adjustedY >= 0 and adjustedX <= 1 and adjustedY <= 1) then
-			adjustedX = math.floor(100 * adjustedX)
-			adjustedY = math.floor(100 * adjustedY)
-			cursorcoords:SetText(MOUSE_LABEL..": "..adjustedX..", "..adjustedY)
-		else
-			cursorcoords:SetText(" ")
-		end
-
-		last = 0
 	end
 end)
 

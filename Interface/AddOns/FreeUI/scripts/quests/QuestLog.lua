6acf68775f3b3a375d74ab048416050fe5ef4ecd
solor <Naushika@outlook.com>
2016-11-10 02:41:57 +0800

    update quest log

--- Interface/AddOns/FreeUI/scripts/quests/QuestLog.lua
+++ Interface/AddOns/FreeUI/scripts/quests/QuestLog.lua
@@ -1,13 +1,18 @@
+local F, C, L = unpack(select(2, ...))
+
 -- Quest level
 hooksecurefunc("QuestLogQuests_Update", function()
 	for i, button in pairs(QuestMapFrame.QuestsFrame.Contents.Titles) do
 		if button:IsShown() then
-			local level = strmatch(GetQuestLink(button.questLogIndex), "quest:%d+:(%d+)")
-			if level then
-				local height = button.Text:GetHeight()
-				button.Text:SetFormattedText("[%d] %s", level, button.Text:GetText())
-				button.Check:SetPoint("LEFT", button.Text, button.Text:GetWrappedWidth() + 2, 0)
-				button:SetHeight(button:GetHeight() - height + button.Text:GetHeight())
+			local link = GetQuestLink(button.questID)
+			if link then
+				local level = strmatch(link, "quest:%d+:(%d+)")
+				if level then
+					local height = button.Text:GetHeight()
+					button.Text:SetFormattedText("[%d] %s", level, button.Text:GetText())
+					button.Check:SetPoint("LEFT", button.Text, button.Text:GetWrappedWidth() + 2, 0)
+					button:SetHeight(button:GetHeight() - height + button.Text:GetHeight())
+				end
 			end
 		end
 	end

acc5b44ed62d113731c56002dcdde12bdd165c78
solor <Naushika@outlook.com>
2016-12-05 05:43:26 +0800

    reorganize :warning:

---
+++ Interface/AddOns/FreeUI/Modules/Quests/QuestLog.lua
@@ -0,0 +1,28 @@
+local F, C, L = unpack(select(2, ...))
+
+
+-- Ctrl+Click to abandon a quest or Alt+Click to share a quest(by Suicidal Katt)
+hooksecurefunc("QuestMapLogTitleButton_OnClick", function(self)
+	local questLogIndex = GetQuestLogIndexByID(self.questID)
+	if IsControlKeyDown() then
+		QuestMapQuestOptions_AbandonQuest(self.questID)
+	elseif IsAltKeyDown() and GetQuestLogPushable(questLogIndex) then
+		QuestMapQuestOptions_ShareQuest(self.questID)
+	end
+end)
+
+hooksecurefunc(QUEST_TRACKER_MODULE, "OnBlockHeaderClick", function(block)
+	local questLogIndex = block.questLogIndex
+	if IsControlKeyDown() then
+		local items = GetAbandonQuestItems()
+		if items then
+			StaticPopup_Hide("ABANDON_QUEST")
+			StaticPopup_Show("ABANDON_QUEST_WITH_ITEMS", GetAbandonQuestName(), items)
+		else
+			StaticPopup_Hide("ABANDON_QUEST_WITH_ITEMS")
+			StaticPopup_Show("ABANDON_QUEST", GetAbandonQuestName())
+		end
+	elseif IsAltKeyDown() and GetQuestLogPushable(questLogIndex) then
+		QuestLogPushQuest(questLogIndex)
+	end
+end)

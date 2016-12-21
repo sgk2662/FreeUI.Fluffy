2b9e87decbb6bcb3d448cb690a71a6c2ad83d6a5
solor <Naushika@outlook.com>
2016-12-22 00:07:51 +0800

    update UIFader

--- Interface/AddOns/FreeUI/Modules/Miscellaneous/UIFader.lua
+++ Interface/AddOns/FreeUI/Modules/Miscellaneous/UIFader.lua
@@ -7,10 +7,10 @@ if not C.appearance.uiFader then return end
 local addonName, addonTable = ... ;
 
 addonTable.TIMETOFADEIN = 1.0;
-addonTable.FADEIN = 1;
+addonTable.FADEIN = 1.0;
 
 addonTable.TIMETOFADEOUT = 3.0;
-addonTable.FADEOUT = 0.25;
+addonTable.FADEOUT = 0.00;
 
 local addon = CreateFrame("Frame");
 
@@ -22,15 +22,18 @@ addon:SetScript("OnUpdate", function()
 		return;
 	end;
 
+	MouseFrame = false;
+	if GetMouseFocus() then
+		MouseFrame = GetMouseFocus():GetName();
+	end;
+
 	if ChatFrame1EditBox:IsShown()
 	or WorldMapFrame:IsShown()
 	or MailFrame:IsShown()
 	or GossipFrame:IsShown()
 	or GameMenuFrame:IsShown()
 	or StaticPopup1:IsShown()
-	--or TalkingHeadFrame:IsShown()
-	--or GarrisonMissionAlertFrame:IsShown()
-	--or WorldQuestCompleteAlertFrame:IsShown()
+	or MirrorTimer1:IsShown()
 	or LFGDungeonReadyPopup:IsShown()
 	or LFDRoleCheckPopup:IsShown()
 	or LevelUpDisplay:IsShown()
@@ -40,22 +43,17 @@ addon:SetScript("OnUpdate", function()
 	or QuestLogPopupDetailFrame:IsShown()
 	or GameTooltipTextLeft1:GetText()
 	or UnitCastingInfo("Player")
+	or UnitCastingInfo("Vehicle")
 	or UnitChannelInfo("Player")
+	or UnitChannelInfo("Vehicle")
 	or UnitExists("Target")
 	or MouseIsOver(ChatFrame1)
 	or MouseIsOver(ChatFrame2)
 	or MouseIsOver(ChatFrame3)
+	or MouseFrame ~= "WorldFrame"
 	or MouseIsOver(ChatFrame4) then
 		UIFrameFadeIn(UIParent, addonTable.TIMETOFADEIN, UIParent:GetAlpha(), addonTable.FADEIN);
-		return;
-	end;
-
-	if GetMouseFocus() then
-
-		if GetMouseFocus():GetName() ~= "WorldFrame" then
-			UIFrameFadeIn(UIParent, addonTable.TIMETOFADEIN, UIParent:GetAlpha(), addonTable.FADEIN);
-		else
-			UIFrameFadeOut(UIParent, addonTable.TIMETOFADEOUT, UIParent:GetAlpha(), addonTable.FADEOUT);
-		end;
+	else
+		UIFrameFadeOut(UIParent, addonTable.TIMETOFADEOUT, UIParent:GetAlpha(), addonTable.FADEOUT);
 	end;
 end);

c430b57702a0eb6bdd7b0056d75088630a21cae8
solor <Naushika@outlook.com>
2017-02-14 23:22:45 +0800

    update DBM timer bar skin
    remove pre-set font setting

--- Interface/AddOns/FreeUI/Modules/Skins/DBM_Bars.lua
+++ Interface/AddOns/FreeUI/Modules/Skins/DBM_Bars.lua
@@ -1,13 +1,4 @@
-local F, C, L = unpack(select(2, ...))
-
-local DBMBarFont
-if GetLocale() == "zhCN" or GetLocale() == "zhTW" then
-	if C.appearance.fontUseChinesePixelFont then
-		DBMBarFont = C.fontCN.pixel
-	else
-		DBMBarFont = C.fontCN.standard
-	end
-end
+local F, C, L = unpack(FreeUI)
 
 local function InitStyle()
 	hooksecurefunc(DBT, "CreateBar", function(self)
@@ -17,32 +8,24 @@ local function InitStyle()
 			local tbar = _G[name]
 			local text = _G[name.."Name"]
 
-			tbar:SetHeight(6)
+			tbar:SetHeight(7)
 
 			text:SetPoint("CENTER", 0, 10)
 			text:SetPoint("LEFT", 2, 10)
 
 			if not bar.styled then
-				local texture = _G[name.."Texture"]
-				local timer = _G[name.."Timer"]
-				local spark = _G[name.."Spark"]
-				local icon = _G[name.."Icon1"]
+				local texture = _G[frame:GetName().."BarTexture"]
+				local timer = _G[frame:GetName().."BarTimer"]
+				local spark = _G[frame:GetName().."BarSpark"]
+				local icon = _G[frame:GetName().."BarIcon1"]
 
 				F.CreateBDFrame(tbar, 0)
 
 				texture:SetTexture(C.media.texture)
 				texture.SetTexture = F.dummy
 
-				F.SetFS(text)
-				if GetLocale() == "zhCN" or GetLocale() == "zhTW" then
-					text:SetFont(unpack(DBMBarFont))
-				end
-				text.SetFont = F.dummy
-
 				timer:SetPoint("CENTER", 0, 10)
 				timer:SetPoint("RIGHT", -2, 10)
-				F.SetFS(timer)
-				timer.SetFont = F.dummy
 
 				spark:SetSize(8, 16)
 				spark.SetSize = F.dummy
@@ -80,96 +63,14 @@ local function InitStyle()
 
 			DBMRangeCheckRadar.background:SetTexture("")
 			F.CreateBDFrame(DBMRangeCheckRadar)
-			F.SetFS(DBMRangeCheckRadar.text)
-			DBMRangeCheckRadar.text:SetTextColor(1, 1, 1)
-			F.SetFS(DBMRangeCheckRadar.inRangeText)
-			DBMRangeCheckRadar.inRangeText:SetTextColor(1, 1, 1)
 
-			if GetLocale() == "zhCN" or GetLocale() == "zhTW" then
-				DBMRangeCheckRadar.text:SetFont(unpack(DBMBarFont))
-				DBMRangeCheckRadar.inRangeText:SetFont(unpack(DBMBarFont))
-			end
+			DBMRangeCheckRadar.text:SetTextColor(1, 1, 1)
+			DBMRangeCheckRadar.inRangeText:SetTextColor(1, 1, 1)
 
 			firstRange = false
 		end
 	end)
 
-	hooksecurefunc(DBM.BossHealth, "Show", function()
-		local anchor = DBMBossHealthDropdown:GetParent()
-		if not anchor.styled then
-			local header = anchor:GetRegions()
-			F.SetFS(header)
-			if GetLocale() == "zhCN" or GetLocale() == "zhTW" then
-				header:SetFont(unpack(DBMBarFont))
-			end
-			header:SetTextColor(1, 1, 1)
-
-			anchor.styled = true
-		end
-	end)
-
-	local count = 1
-
-	local styleBar = function()
-		local bar = _G["DBM_BossHealth_Bar_"..count]
-
-		while bar do
-			if not bar.styled then
-				local name = bar:GetName()
-				local sb = _G[name.."Bar"]
-				local text = _G[name.."BarName"]
-				local timer = _G[name.."BarTimer"]
-
-				_G[name.."BarBackground"]:Hide()
-				_G[name.."BarBorder"]:SetNormalTexture("")
-
-				sb:SetStatusBarTexture(C.media.texture)
-
-				F.SetFS(text)
-				F.SetFS(timer)
-				
-				if GetLocale() == "zhCN" or GetLocale() == "zhTW" then
-					text:SetFont(unpack(DBMBarFont))
-					timer:SetFont(unpack(DBMBarFont))
-				end
-
-				F.CreateBDFrame(sb)
-
-				bar.styled = true
-			end
-
-			count = count + 1
-			bar = _G["DBM_BossHealth_Bar_"..count]
-		end
-	end
-
-	hooksecurefunc(DBM.BossHealth, "AddBoss", styleBar)
-	hooksecurefunc(DBM.BossHealth, "UpdateSettings", styleBar)
-
-	hooksecurefunc(DBM, "ShowUpdateReminder", function()
-		-- no name or anything
-		-- reverse loop because it's most likely to be somewhere at the end
-		for i = UIParent:GetNumChildren(), 1, -1 do
-			local frame = select(i, UIParent:GetChildren())
-
-			local editBox = frame:GetChildren()
-			if editBox and editBox:GetObjectType() == "EditBox" and editBox:GetText() == "http://www.deadlybossmods.com" and not frame.styled then
-				F.CreateBD(frame)
-
-				select(6, editBox:GetRegions()):Hide()
-				select(7, editBox:GetRegions()):Hide()
-				select(8, editBox:GetRegions()):Hide()
-
-				local bg = F.CreateBDFrame(editBox, .25)
-				bg:SetPoint("TOPLEFT", -2, -6)
-				bg:SetPoint("BOTTOMRIGHT", 2, 8)
-
-				F.Reskin(select(2, frame:GetChildren()))
-
-				frame.styled = true
-			end
-		end
-	end)
 end
 
 if IsAddOnLoaded("DBM-Core") then

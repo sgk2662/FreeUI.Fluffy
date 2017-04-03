e12a3b6303396432dcbf2a15f59118976ff37c56
solor <Naushika@outlook.com>
2017-04-03 13:10:14 +0800

    update IDTip
    disable artifact power id for now

--- Interface/AddOns/FreeUI/Modules/Tooltip/IDTip.lua
+++ Interface/AddOns/FreeUI/Modules/Tooltip/IDTip.lua
@@ -74,13 +74,13 @@ GameTooltip:HookScript("OnTooltipSetSpell", function(self)
 end)
 
 -- Artifact Powers
-hooksecurefunc(GameTooltip, "SetArtifactPowerByID", function(self, id)
-	local spellid = C_ArtifactUI.GetPowerInfo(id)
-	if debuginfo == true and id and IsModifierKeyDown() then
-		-- addLine(self, id, types.artifactpower)
-		addLine(self, spellid, types.spell)
-	end
-end)
+-- hooksecurefunc(GameTooltip, "SetArtifactPowerByID", function(self, id)
+-- 	local spellid = C_ArtifactUI.GetPowerInfo(id)
+-- 	if debuginfo == true and id and IsModifierKeyDown() then
+-- 		-- addLine(self, id, types.artifactpower)
+-- 		addLine(self, spellid, types.spell)
+-- 	end
+-- end)
 
 -- Items
 local function attachItemTooltip(self)
